cd879c3e508e37e4ebb49830f3d89282171a13d6
Crowdbotics <team@crowdbotics.com>
2020-07-17 19:54:20 +0000

    Update models

--- backend/abc_legal_services__18982/wsgi.py
+++ backend/abc_legal_services__18982/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abc_legal_services__18982.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abc_legal_services__18982.settings")
 
 application = get_wsgi_application()
